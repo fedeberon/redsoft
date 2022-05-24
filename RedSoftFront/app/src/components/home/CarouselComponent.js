10464338e5410c9c0188c344e3547d26e8bd971b
Pablo Iriarte <piriarte@clicoh.com>
2022-05-24 14:36:37 -0300

    Cambio en etiqueta de video

--- RedSoftFront/app/src/components/home/CarouselComponent.js
+++ RedSoftFront/app/src/components/home/CarouselComponent.js
@@ -1,33 +1,31 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 class CarouselComponent extends Component {
+  state = {
+    x: 0,
+    n: "next",
+    p: "prev",
+  };
 
-    state = {
-        x : 0,
-        n : "next",
-        p : "prev",
+  changeImage(e, value) {
+    e.preventDefault();
+    // console.log(value)
+  }
+
+  componentDidUpdate(prevProps) {
+    // si los props cambiaron
+    if (prevProps.focus !== this.props.focus) {
+      // movemos el carrusel para la izquierda o derecha (-1 izquierda, 1 derecha)
+      // ej. está en 2 y antes estaba en 1 entonces se mueve a la derecha
+      this.move(this.props.focus - prevProps.focus);
     }
+  }
 
-    changeImage(e, value) {
-       e.preventDefault();
-        // console.log(value)
-    }
-
-    componentDidUpdate(prevProps) {
-        // si los props cambiaron
-        if (prevProps.focus !== this.props.focus) {
-          // movemos el carrusel para la izquierda o derecha (-1 izquierda, 1 derecha)
-          // ej. está en 2 y antes estaba en 1 entonces se mueve a la derecha
-          this.move(this.props.focus - prevProps.focus);
-        }
-      }
-
-    render (){
-
-        return (
-            <div className="carousel slide" data-ride="carousel">
-                <div className="carousel-inner">
-                    {/*<div className="carousel-item active">
+  render() {
+    return (
+      <div className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          {/*<div className="carousel-item active">
                         <img style={{cursor: 'pointer'}} src="img/slider-desktop-01.jpg" alt="img1" className="d-block w-100"/>
                     </div>
                     <div className="carousel-item">
@@ -39,13 +37,13 @@ class CarouselComponent extends Component {
                     <div className="carousel-item">
                         <img style={{cursor: 'pointer'}} src="img/slider-desktop-03.png" alt="img3" className="d-block w-100"/>
                     </div>*/}
-                    <div className="carousel-inner">
-                        <video className={`carouselvideo`} autoPlay loop muted>
-                            <source src="videos/laRed.mp4" type="video/mp4"/>
-                        </video>
-                    </div>
-                </div>
-                {/* <a className="carousel-control-prev" href="" role="button" data-slide="prev">
+          <div className="carousel-inner">
+            <video className="carouselvideo" autoPlay loop muted>
+              <source src="videos/laRed.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        {/* <a className="carousel-control-prev" href="" role="button" data-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="sr-only">Previous</span>
                 </a>
@@ -53,8 +51,8 @@ class CarouselComponent extends Component {
                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
                     <span className="sr-only">Next</span>
                 </a> */}
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 export default CarouselComponent;
